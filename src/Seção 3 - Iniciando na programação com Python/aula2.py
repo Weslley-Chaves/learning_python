87590d70a0f44dc2426a3fa5a59a8eb682017e0f
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 13:49:53 -0300

    feat: aula 2 - argumentos nomeados e a função 'print()'

---
+++ src/Seção 3 - Iniciando na programação com Python/aula2.py
@@ -0,0 +1,13 @@
+print(12,34) # Usada para exibir os argumentos no terminal
+teste = 1
+# Os argumentos nomeados podem ser alterados na função
+# A função 'print()' possuem 4 argumentos nomeados
+print(12,34, sep="") # separador
+print(56,78, end=" ") # final de linha
+print("-",12,34)
+
+# Quebra de linha:
+# \r\n -> CRLF
+# \n -> LF
+
+# Em programação existe diferenças entre letras maiúsculas e minúsculas
