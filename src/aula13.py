305e728eac4f908cf783725267b79d2ac74efcd5
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 17:17:12 -0300

    feat: aula 13 - introdução a f-strings

---
+++ src/aula13.py
@@ -0,0 +1,16 @@
+# Introdução a f-strings
+
+
+# Calculadora de IMC simples
+
+nome = "Weslley"
+altura = 1.81
+peso = 65
+imc = peso / (altura * altura)
+
+
+print(f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}')
+
+print(...) # Utilizado para evitar quebrar o código enquanto você ainda não pensou no valor ou no que escrever quando o código esperar um argumento
+
+... # Ellipsis
