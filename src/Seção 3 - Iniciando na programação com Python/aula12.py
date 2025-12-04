36dc024de6ecd645347a4827fc3f7b64d0f5b1db
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 17:08:24 -0300

    feat: aula 12 - mini atividade: Calculando o IMC de forma simples

---
+++ src/Seção 3 - Iniciando na programação com Python/aula12.py
@@ -0,0 +1,13 @@
+# Calculadora de IMC simples
+
+nome = "Weslley"
+altura = 1.81
+peso = 65
+imc = peso / (altura * altura)
+
+print(nome,'tem',altura,'de altura, pesa',peso,'quilos e seu IMC é',imc)
+
+
+print(...) # Utilizado para evitar quebrar o código enquanto você ainda não pensou no valor ou no que escrever quando o código esperar um argumento
+
+... # Ellipsis
