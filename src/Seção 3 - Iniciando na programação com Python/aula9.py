f46534fa47320ba7ef538a2141b424f7ba2b35b2
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 16:36:31 -0300

    feat: aula 9 - operadores lógicos

---
+++ src/Seção 3 - Iniciando na programação com Python/aula9.py
@@ -0,0 +1,33 @@
+# Operadores lógicos
+
+number_1 = 15
+number_2 = 5
+number_3 = 2
+
+
+adicao = number_1 + number_2
+print('Adição', adicao)
+
+subtracao = number_1 - number_2
+print('Subtração', subtracao)
+
+multiplicacao = number_1 * number_3
+print('Multiplicação', multiplicacao)
+
+divisao = number_1 / number_2  # float
+print('Divisão', divisao)
+
+divisao_inteira = number_1 // number_2
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = number_3 ** number_1
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
