29600e0bf696018469685d9adc993cdd639b0a35
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 16:46:00 -0300

    feat: aula 10 - concatenção e repetição com os operadores aritméticos

---
+++ src/Seção 3 - Iniciando na programação com Python/aula10.py
@@ -0,0 +1,11 @@
+# CONCATENAÇÃO - Apenas String
+
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+# REPETIÇÃO - String & Inteiros
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+
+tres_vezes_meu_nome = 3 * 'Weslley'
+print(tres_vezes_meu_nome)
