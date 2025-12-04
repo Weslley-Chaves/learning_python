2d3bcaaf1d69b842e617e8bc90d6a928b71a4b73
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 16:07:18 -0300

    feat: aula 7 - variáveis

---
+++ src/Seção 3 - Iniciando na programação com Python/aula7.py
@@ -0,0 +1,15 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel (snack case) = expressão
+
+nome_completo = "Weslley Santos Chaves"
+idade = 1 + 23
+maior_de_idade = idade >= 18
+
+print('Nome:',nome_completo, 'Idade:', idade, 'Maior de idade?',maior_de_idade,sep="\n")
+
+# print(idade, type(idade), sep=" = ")
+
