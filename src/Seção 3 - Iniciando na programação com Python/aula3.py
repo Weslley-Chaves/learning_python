db08db958d86334b11367700e602ec6194abaf44
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 14:02:23 -0300

    feat: aula 3 - Strings em Python

---
+++ src/Seção 3 - Iniciando na programação com Python/aula3.py
@@ -0,0 +1,18 @@
+""" DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Weslley Chaves')
+
+# Aspas duplas
+print("Weslley Chaves")
+
+# Escape
+print("Weslley \"Santos\"") # O caracter de escape (\) antece o caractere a ser oculto
+
+# r
+print(r"Weslley \"Santos\"") # O caracter de escape pode aparecer
