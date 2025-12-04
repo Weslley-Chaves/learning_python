50823ed6740a1ba4c2ef84760ccc43cefb297c5d
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-04 13:35:26 -0300

    feat: aula 1 - Comentários e DocStrings

---
+++ src/Seção 3 - Iniciando na programação com Python/aula1.py
@@ -0,0 +1,14 @@
+# Esse é um comentário de linha única em Python
+
+""" DocString - Não é considerado um comentário. O interpretador do Python lê esse código.
+Serve para anotações e notas mais complexas de várias linhas em Python. Abaixo exemplo:
+
+Line 1
+Line 2
+Line 3
+"""
+
+# print(1) Comentários não são executados
+print(1 + 1) # Função para imprimir/exibir algo no terminal
+print(123) # Função para imprimir/exibir algo no terminal
+print(456)
