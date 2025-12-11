485ed5415538e38878b3984924e4be9245d8b4b7
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-11 10:13:48 -0300

    feat: aula 14 - aprendendo a utilizar o método .format

--- src/Seção 3 - Iniciando na programação com Python/aula14.py
+++ src/Seção 3 - Iniciando na programação com Python/aula14.py
@@ -1,3 +1,20 @@
 # Formatação de Strings com o método format
 
+a = 'A'
+b = 'B'
+c = 1.1
+formato = 'a={nome1} b={nome2} c={nome3:.2f} d={nome3}'.format(nome1=a, nome2=b, nome3=c) # Podemos criar parâmetros - ao criar um parâmetro, todos em frente a ele precisam também ser nomeados
+# A formatação deve ser declarada dentro da frase
+print(formato)
 
+
+# Exemplo de como utilizar o método format:
+
+nome = 'Weslley'
+sobrenome = 'Santos Chaves'
+idade = 24
+altura = 1.81
+
+frase = 'Meu nome é {0} {1}, tenho {2} anos e {3} de altura.'.format(nome, sobrenome, idade, altura) # Podemos também utilizar argumentos
+
+print(frase)
