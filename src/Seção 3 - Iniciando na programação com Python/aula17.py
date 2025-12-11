5402b3b98e12e71caabbeb11ed7f4698a3ecfd71
Weslley Chaves <weslleysantoschaves@gmail.com>
2025-12-11 11:08:46 -0300

    feat: aula 17 - review da aula 16 - estruturas condicionais

---
+++ src/Seção 3 - Iniciando na programação com Python/aula17.py
@@ -0,0 +1,14 @@
+# if, elif e else; se, se não se e se não
+
+condicao = False
+
+if condicao:
+    print('Esse é o código do if.')
+else:
+    print('Esse é o else.')
+
+if condicao == False:
+    print('If correto')
+
+print('Fora do if')
+
